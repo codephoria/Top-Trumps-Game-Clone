4ac668d1cbaafc92d87b716d9398cf4c775aa817
codephoria <>
2021-06-13 01:17:47 +0200

    Add project files.

---
+++ AnimalTrumpsGame/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalTrumpsGame
+{
+    class Card
+    {
+        public string Name { get; private set; }
+        public string Prop1 { get; private set; }
+        public string Prop2 { get; private set; }
+        public string Prop3 { get; private set; }
+        public string Prop4 { get; private set; }
+
+        public Card(string name, string prop1, string prop2, string prop3, string prop4)
+        {
+            this.Name = name;
+            this.Prop1 = prop1;
+            this.Prop2 = prop2;
+            this.Prop3 = prop3;
+            this.Prop4 = prop4;
+        }
+    }
+}
---
+++ AnimalTrumpsGame/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalTrumpsGame
+{
+    class GameManager
+    {
+        TrumpsGameData gameData;
+
+        public GameManager(TrumpsGameData gameData) {
+
+            // sets initial game data
+            this.gameData = gameData;
+        }
+
+        public void StartNewGame()
+        {
+
+        }
+
+        private void SetUpGame()
+        {
+
+        }
+    }
+}
