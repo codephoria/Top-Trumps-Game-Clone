8aff7700d60b23e7e67257edd9a7874ca9fd97ea
codephoria <>
2021-06-13 01:39:36 +0200

    introduced game data adapter

---
+++ AnimalTrumpsGame/GameDataAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalTrumpsGame
+{
+    static class GameDataAdapter
+    {
+        public static Card[] CreateGameCards(TrumpsGameData gameData)
+        {
+            Card[] cards = new Card[gameData.NumberOfCards];
+
+
+
+            return cards;
+        }
+    }
+}
--- AnimalTrumpsGame/GameManager.cs
+++ AnimalTrumpsGame/GameManager.cs
@@ -7,16 +7,18 @@ namespace AnimalTrumpsGame
     class GameManager
     {
         private TrumpsGameData gameData;
+        private Card[] cardDeck;
 
 
         public void StartNewGame(TrumpsGameData gameData)
         {
             this.gameData = gameData;
+            SetUpGame();
         }
 
         private void SetUpGame()
         {
-
+            cardDeck = GameDataAdapter.CreateGameCards(gameData);
         }
     }
 }
